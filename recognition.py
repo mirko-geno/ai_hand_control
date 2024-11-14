a3b6ce88e318b33f25f661a0ba7279d4e885b4f7
mirko-geno <mirkogenovesi24@gmail.com>
2024-11-14 19:38:54 -0300

    Inserted landmark recognizer inside a class. Improved legibility

--- recognition.py
+++ recognition.py
@@ -1,43 +1,67 @@
 import cv2
 import mediapipe as mp
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_hands = mp.solutions.hands
 
 
+class Hand:
+    def __init__(self):
+        self.mp_hands = mp.solutions.hands
 
-# For webcam input:
-cap = cv2.VideoCapture(0)
-with mp_hands.Hands(
-    model_complexity=0,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
-    while cap.isOpened():
-        success, image = cap.read()
-        if not success:
-            print("Ignoring empty camera frame.")
-            # If loading a video, use 'break' instead of 'continue'.
-            continue
+        # Webcam input
+        self.cap = cv2.VideoCapture(0)
+        # Webcam output
+        self.image = None
+    
 
-        # To improve performance, optionally mark the image as not writeable to
-        # pass by reference.
-        image.flags.writeable = False
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        results = hands.process(image)
+    def print_image(self, results):
+        mp_drawing = mp.solutions.drawing_utils
+        mp_drawing_styles = mp.solutions.drawing_styles
 
-        # Draw the hand annotations on the image.
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        # To draw the hand annotations on the image.
+        self.image.flags.writeable = True
+        self.image = cv2.cvtColor(self.image, cv2.COLOR_RGB2BGR)
         if results.multi_hand_landmarks:
+            results.multi_hand_landmarks[0]
+
             for hand_landmarks in results.multi_hand_landmarks:
                     mp_drawing.draw_landmarks(
-                        image,
+                        self.image,
                         hand_landmarks,
-                        mp_hands.HAND_CONNECTIONS,
+                        self.mp_hands.HAND_CONNECTIONS,
                         mp_drawing_styles.get_default_hand_landmarks_style(),
                         mp_drawing_styles.get_default_hand_connections_style())
         # Flip the image horizontally for a selfie-view display.
-            cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
-            if cv2.waitKey(5) & 0xFF == 27:
-                break
-cap.release()
+        cv2.imshow('MediaPipe Hands', cv2.flip(self.image, 1))
+
+
+    def get_landmarks(self, hands):
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        self.image.flags.writeable = False
+        self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
+        results = hands.process(self.image)
+        return results
+
+
+    def setup(self):
+        with self.mp_hands.Hands(
+            max_num_hands=1,
+            model_complexity=0,
+            min_detection_confidence=0.7,
+            min_tracking_confidence=0.7
+            ) as hands:
+            
+            while self.cap.isOpened():
+                _, self.image = self.cap.read()
+                
+                results = self.get_landmarks(hands)
+
+                self.print_image(results)
+               
+                if cv2.waitKey(5) & 0xFF == 27:
+                    break
+        self.cap.release()
+
+
+if __name__ == '__main__':
+    hand = Hand()
+    hand.setup()
